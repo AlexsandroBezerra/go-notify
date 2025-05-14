9ef1d46bf9d68e6bc317a77cae5e4485b510e5fd
Alexsandro G Bezerra <me@alexsandrobezerra.dev>
2025-05-13 22:06:06 -0300

    Add chi middlewares

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 	r := chi.NewRouter()
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Route("/emails", func(r chi.Router) {
+		r.Get("/", notImplementedHandler)
+		r.Post("/", notImplementedHandler)
 	})
 
-	http.ListenAndServe(":3333", r)
+	err := http.ListenAndServe(":3333", r)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func notImplementedHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("Route not implemented yet"))
 }
